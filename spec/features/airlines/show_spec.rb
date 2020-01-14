3af41d29c8726c273ca1195ba4a86ef06428551e
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-14 09:54:44 -0700

    Update test for story 2

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Airline Show Spec" do
   describe "When I visit the airline show page" do
-    it 'I can see that airlines name and all its flights' do
+    it 'I can see that airlines name, all its flights and all the unique list of passenger for thsoe flights' do
       southwest = Airline.create(name: "Southwest")
 
       southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
       southwest_2 = southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
+
+
+      passenger_1 = Passenger.create(name: "Lucy", age:26)
+      passenger_2 = Passenger.create(name: "Jimmy", age:36)
+      passenger_3 = Passenger.create(name: "Tony", age:2)
+      passenger_4 = Passenger.create(name: "Hannah", age:18)
+      passenger_5 = Passenger.create(name: "David", age:20)
+
+
+      PassengerFlight.create(passenger: passenger_1, flight: southwest_1)
+      PassengerFlight.create(passenger: passenger_2, flight: southwest_1)
+      PassengerFlight.create(passenger: passenger_3, flight: southwest_1)
+      PassengerFlight.create(passenger: passenger_4, flight: southwest_2)
+      PassengerFlight.create(passenger: passenger_5, flight: southwest_2)
 
       visit "/airlines/#{southwest.id}"
 
@@ -18,6 +32,9 @@ RSpec.describe "Airline Show Spec" do
         expect(page).to have_content("Time: #{southwest_1.time}")
         expect(page).to have_content("Departure City: #{southwest_1.departure_city}")
         expect(page).to have_content("Arrival City: #{southwest_1.arrival_city}")
+        expect(page).to have_content(passenger_1.name)
+        expect(page).to have_content(passenger_2.name)
+        expect(page).to have_content(passenger_3.name)
       end
 
       within "#flight-#{southwest_2.id}" do
@@ -26,6 +43,8 @@ RSpec.describe "Airline Show Spec" do
         expect(page).to have_content("Time: #{southwest_2.time}")
         expect(page).to have_content("Departure City: #{southwest_2.departure_city}")
         expect(page).to have_content("Arrival City: #{southwest_2.arrival_city}")
+        expect(page).to have_content(passenger_4.name)
+        expect(page).to have_content(passenger_5.name)
       end
     end
   end
